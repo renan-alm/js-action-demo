a81089f4da9cd398743af655b9403f7f4b89aa55
Renan Alm <xrenan.alm@gmail.com>
2025-04-07 16:46:26 +0200

    Init action

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('user-name');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
