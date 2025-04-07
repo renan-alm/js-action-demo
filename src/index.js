4bcb567524b12aa93328e202f9c22c3c42e7c865
Renan Alm <xrenan.alm@gmail.com>
2025-04-07 16:59:25 +0200

    Init increment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const core = require('@actions/core');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('user-name');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    //
+    const nameToGreet = core.getInput('user-name');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
 } catch (error) {
-  core.setFailed(error.message);
+
+    core.setFailed(error.message);
 }
